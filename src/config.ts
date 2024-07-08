2bf6f92ad7d51a11d058e2bd093d501891b177c1
Jayanthoutnondgenie <jayanth@outbondgenie.com>
2024-07-08 20:47:44 +0530

    resolved bugs

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    MONGO_URI: 'mongodb://localhost:27017/',
+    MONGO_URI: 'mongodb+srv://jcar6768:8wSa2NGbZJAxYwth@test-db-cluster.rotwqil.mongodb.net/?retryWrites=true&w=majority&appName=test-db-cluster',
     PORT: process.env.PORT || 5000
   };
   
